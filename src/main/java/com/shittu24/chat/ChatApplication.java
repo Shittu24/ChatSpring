5ee8b77971bede710f6ebca1624a8a77662d4370
Shittu24 <ibrahimshittu247@gmail.com>
2023-10-20 11:28:33 -0700

    Web Chat App

---
+++ src/main/java/com/shittu24/chat/ChatApplication.java
@@ -0,0 +1,14 @@
+
+package com.shittu24.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/shittu24/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.shittu24.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
